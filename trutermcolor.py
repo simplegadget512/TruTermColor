825e46b2fe217c6600f617c9aaf97fc54f7705ab
SimpleGadget512 <simplegadget512@gmail.com>
2017-09-10 00:45:30 -0500

    Changed how attributes are handled

--- trutermcolor.py
+++ trutermcolor.py
@@ -3,6 +3,18 @@
 
 import os
 import colorutils
+from enum import Enum
+
+
+class Attributes(Enum):
+    BOLD = 1
+    DIM = 2
+    ITALIC = 3
+    UNDERLINED = 4
+#    BLINK = 5
+    REVERSE = 7
+#    INVISIBLE = 8
+#    STRIKETHRU = 9
 
 
 class Color(colorutils.Color):
@@ -22,13 +34,10 @@ Z_FORE = 38
 Z_BACK = 48
 
 
-def _e(color, z_level=Z_FORE, attr_bold=False, attr_italic=False, attr_underlined=False, attr_strikethru=False):
+def _e(color, z_level=Z_FORE, attr=set()):
     """Return escaped background color sequence"""
-    return '\x01{}{}{}{}\x1b[{};2;{};{};{}m\x02'.format(
-        '\x1b[1m' if attr_bold else '',
-        '\x1b[3m' if attr_italic else '',
-        '\x1b[4m' if attr_underlined else '',
-        '\x1b[9m' if attr_strikethru else '',
+    return '\x01{}\x1b[{};2;{};{};{}m\x02'.format(
+        ''.join(['\x1b[{}m'.format(i.value) for i in attr]),
         z_level,
         color.red,
         color.green,
@@ -70,55 +79,46 @@ PALETTE = {
 }
 
 
-def fore_text(txt, fore_color=PALETTE['white'], bold=False, italic=False, underline=False, strikethru=False):
+def fore_text(txt, fore_color=PALETTE['white'], attr=set()):
     return '{}{}{}'.format(
-        _e(fore_color, Z_FORE, bold, italic, underline, strikethru),
+        _e(fore_color, Z_FORE, attr),
         txt,
         _r())
 
 
-def fore_print(txt, fore_color=PALETTE['white'], bold=False, italic=False, underline=False, strikethru=False):
+def fore_print(txt, fore_color=PALETTE['white'], attr=set()):
     print(fore_text(
         txt,
         fore_color,
-        bold,
-        italic,
-        underline,
-        strikethru))
+        attr))
 
 
-def color_text(txt, fore_color=PALETTE['white'], back_color=PALETTE['black'],
-               bold=False, italic=False, underline=False, strikethru=False):
+def color_text(txt, fore_color=PALETTE['white'], back_color=PALETTE['black'], attr=set()):
     return '{}{}{}{}'.format(
-        _e(fore_color, Z_FORE, bold, italic, underline, strikethru),
+        _e(fore_color, Z_FORE, attr),
         _e(back_color, Z_BACK),
         txt,
         _r())
 
 
-def color_print(txt, fore_color=PALETTE['white'], back_color=PALETTE['black'],
-                bold=False, italic=False, underline=False, strikethru=False):
+def color_print(txt, fore_color=PALETTE['white'], back_color=PALETTE['black'], attr=set()):
     print(color_text(
         txt,
         fore_color,
         back_color,
-        bold,
-        italic,
-        underline,
-        strikethru))
+        attr))
 
 
 if __name__ == '__main__':
-    color_print('TruTermColor w/ An Opinionated Palette', PALETTE['white'], PALETTE['black'], bold=True, underline=True)
+    color_print('TruTermColor w/ An Opinionated Palette', PALETTE['white'], PALETTE['black'],
+                {Attributes.BOLD, Attributes.UNDERLINED})
     for fg in PALETTE:
         # print the color names and their components
         c_demo = ''.join([color_text(' \u25CF ', PALETTE[fg], PALETTE[bg]) for bg in PALETTE])
-        c_name = fore_text('{:<12}:'.format(fg, str(PALETTE[fg])), PALETTE[fg], bold=True)
+        c_name = fore_text('{:<12}:'.format(fg, str(PALETTE[fg])), PALETTE[fg], {Attributes.BOLD})
         print('{} {} {}'.format(c_demo, c_name, PALETTE[fg]))
     print()
-    color_print('Text Attributes', PALETTE['white'], PALETTE['black'], bold=True, underline=True)
+    color_print('Text Attributes', PALETTE['white'], PALETTE['black'], {Attributes.BOLD, Attributes.UNDERLINED})
     fore_print('NOTE: Some of the following codes may not be implemented in your terminal software', PALETTE['white'])
-    fore_print('Bold text', PALETTE['white'], bold=True)
-    fore_print('Underline text', PALETTE['white'], underline=True)
-    fore_print('Strikethru text', PALETTE['white'], strikethru=True)
-    fore_print('Italic text', PALETTE['white'], italic=True)
+    for a in Attributes:
+        fore_print('{}'.format(a.name.capitalize()), PALETTE['white'], {a})
